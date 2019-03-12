a42c418f9a35057fdfa1a7373387e5368317fe7a
haogeyao <894944672@qq.com>
2019-03-12 23:04:05 +0800

    tijiao

---
+++ app/Http/Controllers/Admin/RBAC/UserManagementController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Admin\RBAC;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class UserManagementController extends Controller
+{
+    
+    protected $user;
+    public function __construct()
+    {
+     //  $this->middleware('auth');
+     //  $this->middleware('admin-auth')->only('admin');
+      // $this->middleware('team-member')->except('admin');
+    }
+    //显示所有用户
+    public function index()
+    {
+
+        return view('/admin/config');		 
+
+    }
+    public function home()
+    {
+        return "hello world!";
+    }
+ 
+}
